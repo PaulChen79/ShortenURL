de4d9d8839c9cf2e3d0ae5be9a2098d109a5f6ae
Pin Hsu Chen <paul791111@gmail.com>
2022-05-22 12:36:12 +0800

    modify eslint setting on node

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,14 +1,17 @@
-const mongoose = require("mongoose")
-require("dotenv").config()
+const mongoose = require('mongoose')
+require('dotenv').config()
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGODB_URI, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+})
 const db = mongoose.connection
 
 db.on('error', () => {
-    console.log('mongodb error!')
+	console.log('mongodb error!')
 })
 db.once('open', () => {
-    console.log('mongodb connected!')
+	console.log('mongodb connected!')
 })
 
 module.exports = db
